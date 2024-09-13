5460ab6f7ee0143011abfa4f00f247da3e882b8f
Adnan Shafi <adnan.s@kodextech.com>
2024-09-13 20:40:21 +0400

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,20 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:4321'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
+];
